abe9c813c113cf72a890d30d9cd951c5b4117a24
paulass97 <paula.skurczynska@gmail.com>
2020-10-29 14:00:41 +0000

    zad1

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
+
+class Wektor2D
+{
+public:
+    Wektor2D(double xx, double yy)
+    {
+        x = xx;
+        y = yy;
+    }
+    void print() { cout << "x = " << x << '\n' << "y = " << y << endl; }
+
+private:
+    double x;
+    double y;
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D v1(10, 20);
+    v1.print();
+    // v1.norm();
 }
