1743b291345b975f479d997c08984b43c0844bc9
paulass97 <paula.skurczynska@gmail.com>
2020-11-02 17:10:36 +0000

    zad2

--- lab1.cpp
+++ lab1.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <math.h>
+
 using namespace std;
 
-struct Wektor2D
+class Wektor2D
 {
-    vector< vector< double > > wektor{{x, y}};
-    void                       print() { cout << "x = " << x << '\n' << "y = " << y << endl; }
-    void                       norm() { cout << "norma wektora = " << norma << endl; }
+public:
+    Wektor2D(double xx, double yy)
+    {
+        x = xx;
+        y = yy;
+        cout << "x: " << x << "\ny:" << y << endl;
+    }
 
+    ~Wektor2D() { cout << "destruktor x:" << x << "\n y:" << y << endl; }
+
+private:
     double x;
     double y;
-    double norma = abs(x) + abs(y);
 };
 
 int main()
 {
-    Wektor2D v1;
-    v1.x = 10;
-    v1.y = 20;
-    v1.print();
-    v1.norm();
+    Wektor2D v1(20, 10);
+    Wektor2D v2(2.3, 2);
+    Wektor2D v3(1, 5);
 }
